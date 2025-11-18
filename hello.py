fcc1bc61fd765f425976beab2255079cc326057b
yousifalassaly <yousifalassaly01@gmail.com>
2025-11-18 08:54:54 -0700

    changes

--- hello.py
+++ hello.py
@@ -1,10 +1,7 @@
 print("Hello, Git!")
 print("hi")
 print("this is an edit")
-<<<<<<< HEAD
 print("Hello from VS Code!")
 print("This version was edited locally")
-=======
 print("Greetings from GitHub!")
 print("This version was edited on the web")
->>>>>>> refs/remotes/origin/main
